eed04830440d15d6e501c5682262ab639624549d
Damien FREMONT <damien.fremont@gmail.com>
2024-04-21 15:54:01 +0200

    fix: 🐛 pictures - mobile

--- platform/twitter-click-home.sikuli/twitter-click-home.py
+++ platform/twitter-click-home.sikuli/twitter-click-home.py
@@ -1,5 +1,7 @@
-
-if exists("twitter-home-off.png"):
+if exists("twitter-home-on.png"):
+    do_nothing = 1
+    
+elif exists("twitter-home-off.png"):
     click("twitter-home-off.png")
 
 wait("twitter-home-on.png")
