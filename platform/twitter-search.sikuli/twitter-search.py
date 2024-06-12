89e2a77867e99ef8436ace87739ec3084eb69f0c
Damien FREMONT <damien.fremont@gmail.com>
2024-06-12 10:12:08 +0200

    fix: 🐛 search script

--- platform/twitter-search.sikuli/twitter-search.py
+++ platform/twitter-search.sikuli/twitter-search.py
@@ -11,8 +11,8 @@ if exists("20240420_180530-screenshot.png"):
 
 sleep(1)
 
-if exists("20240420_173932-screenshot.png"):
-    click("20240420_173932-screenshot.png")
+if exists("20240612_095406-screenshot.png"):
+    click("20240612_095406-screenshot.png")
 
 sleep(1)
 
