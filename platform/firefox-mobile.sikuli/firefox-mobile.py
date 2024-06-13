70f8a03ff20aae5c1627935f0b59a7cbe03f3032
Damien FREMONT <damien.fremont@gmail.com>
2024-06-13 15:02:07 +0200

    fix: 🐛 shortcut - type faster

--- platform/firefox-mobile.sikuli/firefox-mobile.py
+++ platform/firefox-mobile.sikuli/firefox-mobile.py
@@ -1,12 +1,2 @@
-if exists("20240420_175941-screenshot.png"):
-    do_nothing = 1
-else:
-    keyDown(Key.CTRL)
-    keyDown(Key.SHIFT)
-    keyDown("m")
-    
-    keyUp(Key.CTRL)
-    keyUp(Key.SHIFT)
-    keyUp("m")
-    
-    wait("20240420_175941-screenshot.png")
+type("m", KeyModifier.CTRL + KeyModifier.SHIFT)
+wait("20240420_175941-screenshot.png")
