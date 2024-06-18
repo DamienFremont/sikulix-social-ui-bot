771e070a2f264136764f13355002fd31dfd19ffc
Damien FREMONT <damien.fremont@gmail.com>
2024-06-18 14:49:09 +0200

    feat: 🎸 mobile - reset scale

--- platform/firefox-mobile.sikuli/firefox-mobile.py
+++ platform/firefox-mobile.sikuli/firefox-mobile.py
@@ -1,2 +1,6 @@
 type("m", KeyModifier.CTRL + KeyModifier.SHIFT)
-wait("20240420_175941-screenshot.png")
+
+#wait("20240420_175941-screenshot.png")
+wait("20240618_143826-screenshot.png")
+
+type("0", KeyModifier.CTRL)
