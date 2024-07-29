39207162c444a82a2b1db790eff10f3049ca2321
Damien FREMONT <damien.fremont@gmail.com>
2024-07-29 14:52:22 +0200

    feat: 🎸 facebook - post vid

---
+++ platform/facebook-post-vid.sikuli/facebook-post-vid.py
@@ -0,0 +1,47 @@
+import sys.argv
+
+arg1_title = "20210117_205357 #pet #cat #tabbycat @frimoussethecat (o.o)"
+arg2_img_dir = "file://///192.168.8.2/workdir/project-frimousse-social/"
+arg3_img_fn ="20210117_205357-x265_new.mp4"
+
+# parameters
+if len(sys.argv) > 1:
+    arg1_title = sys.argv[1]
+    arg2_img_dir = sys.argv[2]
+    arg3_img_fn = sys.argv[3]
+
+# post
+click("fb-post-create.png")
+sleep(2)
+
+# add text
+type(arg1_text)
+
+# add pict
+click("fb-post-upload.png")
+sleep(1)
+
+runScript("firefox-file-upload", arg2_img_dir, arg3_img_fn)
+sleep(2)
+
+# submit
+type(Key.TAB)
+type(Key.TAB)
+type(Key.TAB)
+type(Key.TAB)
+type(Key.TAB)
+type(Key.TAB)
+type(Key.TAB)
+type(Key.TAB)
+type(Key.TAB)
+type(Key.TAB)
+type(Key.TAB)
+# TODO type(Key.ENTER)
+
+# SUCCESS
+# TODO
+
+# ERROR: already reposted, cancel
+# TODO
+
+sleep(1)
--- standalone/social-bot-@frimoussethecat-post-vids.sikuli/social-bot-@frimoussethecat-post-vids.py
+++ standalone/social-bot-@frimoussethecat-post-vids.sikuli/social-bot-@frimoussethecat-post-vids.py
@@ -2,7 +2,7 @@
 
 # CONST
 ARG_POST_ID = "20210117_205357"
-ARG_TXT = ARG_POST_ID + " #pet #cat #tabbycat @frimoussethecat (o.o)"
+ARG_TITLE = ARG_POST_ID + " #pet #cat #tabbycat @frimoussethecat (o.o)"
 ARG_IMG_DIR = "file://///192.168.8.2/workdir/project-frimousse-social"
 ARG_IMG_NAM = ARG_POST_ID + "-x265_new.mp4"
 ARG_IMG = ARG_IMG_DIR + "/" + ARG_IMG_NAM
@@ -17,26 +17,26 @@ try:
     # youtube
     runScript("../platform/firefox-container-open", ARG_FFC_ID)
     runScript("../platform/youtube-open")
-    runScript("../platform/youtube-post-vid", ARG_TXT, ARG_IMG_DIR, ARG_IMG_NAM)
+    runScript("../platform/youtube-post-vid", ARG_TITLE, ARG_IMG_DIR, ARG_IMG_NAM)
     
     # twitter
     #runScript("../platform/firefox-container-open", ARG_FFC_ID)
     #runScript("../platform/firefox-mobile")
     #runScript("../platform/twitter-open")
     #runScript("../platform/firefox-clipboard-picture", ARG_IMG)
-    # TODO runScript("../platform/twitter-post-vid", ARG_TXT, ARG_IMG_DIR, ARG_IMG_NAM)
+    #runScript("../platform/twitter-post-vid", ARG_TITLE, ARG_IMG_DIR, ARG_IMG_NAM)
         
     # facebook
-    #runScript("../platform/firefox-container-open", ARG_FFC_ID)
-    #runScript("../platform/firefox-mobile")
-    #runScript("../platform/facebook-open")
-    # TODO runScript("../platform/facebook-post-vid", ARG_TXT, "true")
+    runScript("../platform/firefox-container-open", ARG_FFC_ID)
+    runScript("../platform/firefox-mobile")
+    runScript("../platform/facebook-open")
+    runScript("../platform/facebook-post-vid", ARG_TITLE, ARG_IMG_DIR, ARG_IMG_NAM)
 
     # vimeo
     #runScript("../platform/firefox-container-open", ARG_FFC_ID)
     #runScript("../platform/firefox-mobile")
     #runScript("../platform/vimeo-open")
-    # TODO runScript("../platform/vimeo-post-vid", ARG_TXT, "true")
+    # TODO runScript("../platform/vimeo-post-vid", ARG_TITLE, ARG_IMG_DIR, ARG_IMG_NAM)
     
     # end ***********************************************  
     runScript("../platform/windows-takescreenshot", "-success") 
