7653ea8d752bc271022eef69834594025f378d42
Damien FREMONT <damien.fremont@gmail.com>
2024-07-30 16:17:55 +0200

    fix: 🐛 mastodon - post pic - submit

--- platform/mastodon-post.sikuli/mastodon-post.py
+++ platform/mastodon-post.sikuli/mastodon-post.py
@@ -1,6 +1,6 @@
 import sys.argv
 
-arg1_text = "20210112_233929 #pet #cat #tabbycat @frimoussethecat (o.o)"
+arg1_text = "20210123_000517 #pet #cat #tabbycat @frimoussethecat (o.o)"
 arg2_clip = "true"
 
 # parameters
@@ -9,11 +9,7 @@ if len(sys.argv) > 1:
     arg2_clip = sys.argv[2]
 
 # post
-type(Key.TAB)
-type(Key.TAB)
-type(Key.TAB)
-type(Key.TAB)
-type(Key.TAB)
+click("mastodon-post-create.png")
 
 # add text
 type(arg1_text)
@@ -21,10 +17,15 @@ type(arg1_text)
 # add pict
 if arg2_clip.lower() == "true":
     type("v", KeyModifier.CTRL)
-    sleep(5)
+    sleep(3)
+    wait("mastodon-post-submit-2.png", 120)
 
 # submit
-click("mastodon-post-submit.png")
+click("mastodon-post-submit-2.png")
+sleep(1)
+
+# check upload
+wait("mastodon-post-check.png", 120)
 
 # SUCCESS
 # TODO
@@ -32,4 +33,5 @@ click("mastodon-post-submit.png")
 # ERROR: already reposted, cancel
 # TODO
 
-sleep(1)
+
+
