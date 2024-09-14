0221df621cc01cfcc85874e7647cbdd849083ab9
Damien FREMONT <damien.fremont@gmail.com>
2024-09-14 11:31:38 +0200

    fix: 🐛 cara - post pic - faster

--- platform/cara-post.sikuli/cara-post.py
+++ platform/cara-post.sikuli/cara-post.py
@@ -1,8 +1,8 @@
 import sys.argv
 
-arg1_text = "20210112_233929 #pet #cat #tabbycat @frimoussethecat (o.o)"
+arg1_text = "20211220_005455 #pet #cat #tabbycat @frimoussethecat (o.o)"
 arg2_img_dir = "file://///192.168.8.2/workdir/project-frimousse-social/"
-arg3_img_fn ="20210112_233929-compressed.jpg"
+arg3_img_fn ="20211220_005455-compressed.jpg"
 
 # parameters
 if len(sys.argv) > 1:
@@ -16,7 +16,7 @@ sleep(1)
 
 # type text
 type(Key.TAB)
-type(arg1_text)
+paste(arg1_text)
 
 # upload picutre
 click("cara-post-upload.png")
