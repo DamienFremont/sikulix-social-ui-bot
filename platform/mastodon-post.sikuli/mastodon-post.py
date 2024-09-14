d96c93e8ec9ae9cb146c64a9b60719e149a398a1
Damien FREMONT <damien.fremont@gmail.com>
2024-09-14 10:59:08 +0200

    fix: 🐛 mastodon - post pic - uploaded

--- platform/mastodon-post.sikuli/mastodon-post.py
+++ platform/mastodon-post.sikuli/mastodon-post.py
@@ -1,12 +1,14 @@
 import sys.argv
 
-arg1_text = "20210222_181334 #pet #cat #tabbycat @frimoussethecat (o.o)"
-arg2_clip = "true"
+arg1_text = "20211220_005455 #pet #cat #tabbycat @frimoussethecat (o.o)"
+arg2_img_dir = "file://///192.168.8.2/workdir/project-frimousse-social/"
+arg3_img_fn ="20211220_005455-compressed.jpg"
 
 # parameters
 if len(sys.argv) > 1:
     arg1_text = sys.argv[1]
-    arg2_clip = sys.argv[2]
+    arg2_img_dir = sys.argv[2]
+    arg3_img_fn = sys.argv[3]
 
 # post
 click("mastodon-post-create.png")
@@ -14,13 +16,22 @@ click("mastodon-post-create.png")
 # add text
 type(arg1_text)
 
-# add pict
-if arg2_clip.lower() == "true":
-    type("v", KeyModifier.CTRL)
-    sleep(2)
-    wait("mastodon-post-submit-2.png", 120)
-    sleep(2)
- 
+# upload picutre
+click("mastodon-post-upload.png")
+sleep(1)
+
+# select picture
+runScript("firefox-file-upload", arg2_img_dir, arg3_img_fn)
+sleep(1)
+
+# FIXME: not diff between loading and loaded post buttons
+wait("mastodon-post-uploading.png", 10)
+while(True):
+    if exists("mastodon-post-uploading.png"):
+        wait(1)
+    else:
+        break
+
 # submit
 click("mastodon-post-submit-2.png")
 sleep(1)
