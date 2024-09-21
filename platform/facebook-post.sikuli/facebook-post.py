d73518a77ab3fdbd1ed7e5513cf71999ddeb9769
Damien FREMONT <damien.fremont@gmail.com>
2024-09-21 23:28:49 +0200

    fix: 🐛 facebook - post - confirm

--- platform/facebook-post.sikuli/facebook-post.py
+++ platform/facebook-post.sikuli/facebook-post.py
@@ -1,8 +1,8 @@
 import sys.argv
 
-arg1_text = "20211230_234721 #pet #cat #tabbycat @frimoussethecat (o.o)"
+arg1_text = "20211231_999999 #pet #cat #tabbycat @frimoussethecat (o.o)"
 arg2_img_dir = "file://///192.168.8.2/workdir/project-frimousse-social/"
-arg3_img_fn ="20211230_234721-compressed.jpg"
+arg3_img_fn ="20211231_999999-compressed.jpg"
 
 # parameters
 if len(sys.argv) > 1:
@@ -11,34 +11,33 @@ if len(sys.argv) > 1:
     arg3_img_fn = sys.argv[3]
 
 try:
+
+    # page loaded
+    wait("fb-post-create.png", 30)
     
     # post
     click("fb-post-create.png")
     sleep(1)
     
-    wait("facebook-post-form.png")
+    wait("facebook-post-form.png", 10)
       
     # add text
     paste(arg1_text)
     
+    sleep(1)
+        
     # upload picture
     type(Key.TAB)
     type(Key.TAB)
     type(Key.ENTER)
-    sleep(1)
+    sleep(2)
         
     # select picture
     runScript("firefox-file-upload", arg2_img_dir, arg3_img_fn)
-    
-    sleep(5)
+    sleep(1)
         
     # submit
-    type(Key.TAB, KEY_SHIFT)
-    type(Key.TAB, KEY_SHIFT)
-    type(Key.TAB, KEY_SHIFT)
-    type(Key.TAB, KEY_SHIFT)
-    type(Key.TAB, KEY_SHIFT)
-    type(Key.ENTER)
+    click("facebook-post-confirm.png")
     
     # SUCCESS
     # TODO
