1fa0b11394f972e1bacca3bc91b302a933380d51
Damien FREMONT <damien.fremont@gmail.com>
2025-09-07 16:45:19 +0200

    fix: 🐛 twitter - fixed text area

--- platform/twitter-post.sikuli/twitter-post.py
+++ platform/twitter-post.sikuli/twitter-post.py
@@ -1,7 +1,7 @@
 import sys.argv
 
-ARG_TXT = "20220730_220243 #pet #cat #tabbycat #france @frimoussethecat (o.o)"
-ARG_IMG_FN = "file:c:/Users/damien/SynologyDrive/workdir/project-frimousse-social/20220730_220243-compressed.jpg"
+ARG_TXT = "20220904_185838 #pet #cat #tabbycat #france @frimoussethecat (o.o)"
+ARG_IMG_FN = "file:c:/Users/damien/SynologyDrive/workdir/project-frimousse-social/20220904_185838-compressed.jpg"
 
 # parameters
 if len(sys.argv) > 1:
@@ -15,7 +15,9 @@ try:
     sleep(1)
 
     # add text
+    click("twitter-post-text-area.png")
     paste(ARG_TXT)
+    sleep(1)
 
     # add pict
     click("twitter-post-upload.png")
