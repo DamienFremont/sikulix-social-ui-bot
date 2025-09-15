fa7b3ee3b477749f824eaf540b5552f1d89c1154
Damien FREMONT <damien.fremont@gmail.com>
2025-09-15 08:13:04 +0200

    feat: 🎸 linkedin - updated get profile screenshot

--- platform/linkedin-profile-screenshot-head.sikuli/linkedin-profile-screenshot-head.py
+++ platform/linkedin-profile-screenshot-head.sikuli/linkedin-profile-screenshot-head.py
@@ -2,11 +2,14 @@
 import sys.argv
 import shutil
 
+# CONST
 ARG_1 = "test-head.png"
+ARG_VERTICAL_MARGIN = "0"
 
 # parameters
 if len(sys.argv) > 1:
     ARG_1 = sys.argv[1]
+    ARG_VERTICAL_MARGIN = sys.argv[2]
 
 # RESET ZOOM
 type("0", Key.CTRL)
@@ -15,13 +18,23 @@ sleep(0.5)
 # TAKE SCREENSHOTS
 screen = Screen()
 
-# HEADER
 type(Key.HOME)
 sleep(0.5)
-region = Region(390, 190, 1190 - 390, 668 - 190)
+
+x1 = 390
+y1 = 78
+x2 = 1190
+y2 = 555
+
+x = x1
+y = y1 + int(ARG_VERTICAL_MARGIN)
+w = x2 - x1
+h = y2 - y
+
+region = Region(x, y, w, h)
 file = screen.saveCapture(region)
 
 # moves img to destination
-shutil.move(file, file + "-head.png")
+shutil.move(file, file + "-" + ARG_1)
 
 print("Saved screen as " + file)
