00557995bd1ffe27e783bf5782146aea620148b9
Damien FREMONT <damien.fremont@gmail.com>
2025-10-11 14:17:00 +0200

    feat: 🎸 windows fullwindow

---
+++ platform/windows-fullwindow.sikuli/windows-fullwindow.py
@@ -0,0 +1,24 @@
+# WAIT READY
+sleep(0.5)
+
+# KEYBOARD VERSION
+type(Key.UP, Key.WIN)
+sleep(0.3)
+type(Key.ESC)
+sleep(0.3)
+
+type(Key.UP, Key.WIN)
+sleep(0.3)
+type(Key.ESC)
+sleep(0.3)
+
+type(Key.UP, Key.WIN)
+sleep(0.3)
+type(Key.ESC)
+sleep(0.3)
+
+type(Key.DOWN, Key.WIN)
+sleep(0.3)
+
+type(Key.UP, Key.WIN)
+sleep(0.3)
