c05e2e1c9b9c72e401a18ab982a1d0f0dbdeedaa
Damien FREMONT <damien.fremont@gmail.com>
2025-10-11 13:38:32 +0200

    ci: 🎡 change workdir

--- standalone/social-bot-@frimoussethecat-post-pics.sikuli/social-bot-@frimoussethecat-post-pics.py
+++ standalone/social-bot-@frimoussethecat-post-pics.sikuli/social-bot-@frimoussethecat-post-pics.py
@@ -1,15 +1,13 @@
 # NOTE: Press Alt+Shift+C to to kill a running Sikuli script.
 
 # CONST
-ARG_POST_ID = "20220730_220243"
+ARG_POST_ID = "20221002_221638"
 ARG_TXT = ARG_POST_ID + " #pet #cat #tabbycat #france @frimoussethecat (o.o)"
-ARG_IMG_FN = "file:c:/Users/damien/SynologyDrive/workdir/project-frimousse-social/" + ARG_POST_ID + "-compressed.jpg"
+ARG_IMG_FN = "file:c:/workdir/project-frimousse-social/" + ARG_POST_ID + "-compressed.jpg"
 ARG_FFC_ID = "4"
 
 try:
     # start ***********************************************
-
-    # POST ************************************************
     
     # browser
     runScript("../platform/cmd-run", 'firefox')
