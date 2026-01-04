e8a52beb4f5c896bbf0b4346ea0c3dbaec6d5750
Damien FREMONT <damien.fremont@gmail.com>
2026-01-04 15:44:59 +0100

    feat: 🎸 pinterest - select desc and board

--- platform/pinterest-post-pic.sikuli/pinterest-post-pic.py
+++ platform/pinterest-post-pic.sikuli/pinterest-post-pic.py
@@ -1,14 +1,18 @@
 import sys.argv
 
-ARG_TXT = "20110001 #pet #cat #tabbycat #france @frimoussethecat (o.o)"
-ARG_IMG_FN = "file:c:/Users/damien/workspace/project-frimousse-social/20110001-compressed.jpg"
+ARG_TITLE = "TEST #pet #cat #tabbycat #france @frimoussethecat (o.o)"
+ARG_IMAGE = "file:c:/Users/damien/workspace/project-frimousse-social/folder.jpg"
+ARG_DESCR = "20110001 #pet #cat #tabbycat #france @frimoussethecat (o.o https://linktr.ee/frimoussethecat)"
 
 # parameters
 if len(sys.argv) > 1:
-    ARG_TXT = sys.argv[1]
-    ARG_IMG_FN = sys.argv[2]
-    
+    ARG_TITLE = sys.argv[1]
+    ARG_IMAGE = sys.argv[2]
+    ARG_DESCR = sys.argv[3]
+     
 try:
+    # TODO: wait for create and esc focus
+    
     # post
     click("pinterest-create-button.png")
     sleep(1)
@@ -21,12 +25,23 @@ try:
     sleep(1)
 
     # select picture
-    runScript("firefox-file-upload-2", ARG_IMG_FN)
+    runScript("firefox-file-upload-2", ARG_IMAGE)
     sleep(3)
 
-    # add text
+    # add title
     click("pinterest-create-title.png")
-    paste(ARG_TXT)
+    paste(ARG_TITLE)
+    sleep(1)
+
+    # add description
+    click("pinterest-post-form-description.png")
+    paste(ARG_DESCR)
+    sleep(1)
+
+    # select board
+    click("pinterest-post-form-board.png")
+    sleep(1)
+    click("pinterest-post-form-board-selector.png")
     sleep(1)
 
     # submit
