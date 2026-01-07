f9ec518d6f3595477a4f9b92cf88327d72217a2c
Damien FREMONT <damien.fremont@gmail.com>
2026-01-07 13:12:30 +0100

    fix: 🐛 pinterest - unstable loading on new tab

--- platform/pinterest-open.sikuli/pinterest-open.py
+++ platform/pinterest-open.sikuli/pinterest-open.py
@@ -1,5 +1,14 @@
-type("l", KeyModifier.CTRL)
+ARG_URL = "https://www.pinterest.com/frimoussethecat/_created/"
 
-# start twitter
-paste("https://www.pinterest.com/frimoussethecat/_created/")
+# focus navbar
+type("l", KeyModifier.CTRL)
+sleep(0.5)
+
+# copy URL
+paste(ARG_URL)
 type(Key.ENTER)
+sleep(2)
+
+# unfocus navbar
+type(Key.ESC)
+sleep(1)
