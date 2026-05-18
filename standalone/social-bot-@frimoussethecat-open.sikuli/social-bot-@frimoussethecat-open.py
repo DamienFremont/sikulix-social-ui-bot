879f4457f87dbd0ba53a50af20484f46559241f8
Damien FREMONT <damien.fremont@gmail.com>
2026-05-18 11:04:49 +0200

    refactor: 🔧 streamline URL handling by consolidating constants and removing redundant code

--- standalone/social-bot-@frimoussethecat-open.sikuli/social-bot-@frimoussethecat-open.py
+++ standalone/social-bot-@frimoussethecat-open.sikuli/social-bot-@frimoussethecat-open.py
@@ -1,54 +1,53 @@
 # NOTE: Press Alt+Shift+C to to kill a running Sikuli script.
 
 # CONST
-ARG_FFC_ID = "4"
-ARG_FFC_ID_2 = "3"
-ARG_FFC_ID_3 = "8"
+CONT_FRIMOUSSE = "4"
+CONT_INDY = "5"
+CONT_STREETBOOZE = "6"
 
 try:
     # start ***********************************************
     
     # browser
     runScript("../platform/cmd-run", 'firefox')
+    
+    # main Account
+    runScript("../platform/firefox-cont-new-url", CONT_FRIMOUSSE, "https://www.facebook.com/frimoussethecat/")
+    runScript("../platform/firefox-cont-new-url", CONT_FRIMOUSSE, "https://www.youtube.com/@FrimousseTheCat")
+    runScript("../platform/firefox-cont-new-url", CONT_FRIMOUSSE, "https://www.tiktok.com/@frimousse_the_cat")
+    runScript("../platform/firefox-cont-new-url", CONT_FRIMOUSSE, "https://www.instagram.com/frimousse_the_cat/")
+    runScript("../platform/firefox-cont-new-url", CONT_FRIMOUSSE, "https://x.com/frimoussethecat/")
+    runScript("../platform/firefox-cont-new-url", CONT_FRIMOUSSE, "https://fr.pinterest.com/frimoussethecat/_pins/")
+    runScript("../platform/firefox-cont-new-url", CONT_FRIMOUSSE, "https://imgur.com/user/frimoussethecat/")
+    runScript("../platform/firefox-cont-new-url", CONT_FRIMOUSSE, "https://www.tumblr.com/frimoussethecat")
+    runScript("../platform/firefox-cont-new-url", CONT_FRIMOUSSE, "https://www.threads.net/@frimousse_the_cat")
+    runScript("../platform/firefox-cont-new-url", CONT_FRIMOUSSE, "https://www.flickr.com/photos/frimoussethecat/")
+    runScript("../platform/firefox-cont-new-url", CONT_FRIMOUSSE, "https://mastodon.social/@frimoussethecat")
+    runScript("../platform/firefox-cont-new-url", CONT_FRIMOUSSE, "https://cara.app/frimoussethecat")
+    runScript("../platform/firefox-cont-new-url", CONT_FRIMOUSSE, "https://www.linkedin.com/in/frimoussethecat/")
+    runScript("../platform/firefox-cont-new-url", CONT_FRIMOUSSE, "https://bsky.app/profile/frimoussethecat.bsky.social")
+    runScript("../platform/firefox-cont-new-url", CONT_FRIMOUSSE, "https://vimeo.com/frimoussethecat")
 
-    # @MainAccount
-    runScript("../platform/firefox-cont-new-url", ARG_FFC_ID, "https://www.facebook.com/frimoussethecat/")
-    runScript("../platform/firefox-cont-new-url", ARG_FFC_ID, "https://www.youtube.com/@FrimousseTheCat")
-    runScript("../platform/firefox-cont-new-url", ARG_FFC_ID, "https://www.tiktok.com/@frimousse_the_cat")
-    runScript("../platform/firefox-cont-new-url", ARG_FFC_ID, "https://www.instagram.com/frimousse_the_cat/")
-    runScript("../platform/firefox-cont-new-url", ARG_FFC_ID, "https://x.com/frimoussethecat/")
-    runScript("../platform/firefox-cont-new-url", ARG_FFC_ID, "https://fr.pinterest.com/frimoussethecat/_pins/")
-    runScript("../platform/firefox-cont-new-url", ARG_FFC_ID, "https://imgur.com/user/frimoussethecat/")
-    runScript("../platform/firefox-cont-new-url", ARG_FFC_ID, "https://www.tumblr.com/frimoussethecat")
-    runScript("../platform/firefox-cont-new-url", ARG_FFC_ID, "https://www.threads.net/@frimousse_the_cat")
-    runScript("../platform/firefox-cont-new-url", ARG_FFC_ID, "https://www.flickr.com/photos/frimoussethecat/")
-    runScript("../platform/firefox-cont-new-url", ARG_FFC_ID, "https://mastodon.social/@frimoussethecat")
-    runScript("../platform/firefox-cont-new-url", ARG_FFC_ID, "https://cara.app/frimoussethecat")
-    runScript("../platform/firefox-cont-new-url", ARG_FFC_ID, "https://www.linkedin.com/in/frimoussethecat/")
-    runScript("../platform/firefox-cont-new-url", ARG_FFC_ID, "https://bsky.app/profile/frimoussethecat.bsky.social")
-    runScript("../platform/firefox-cont-new-url", ARG_FFC_ID, "https://vimeo.com/frimoussethecat")
-
-    # @Account1
     runScript("../platform/firefox-tab-new-url", "https://www.facebook.com/frimoussethecat/")
     runScript("../platform/firefox-tab-new-url", "https://www.youtube.com/@FrimousseTheCat")
+    runScript("../platform/firefox-tab-new-url", "https://x.com/frimoussethecat/")    
     runScript("../platform/firefox-tab-new-url", "https://mastodon.social/@frimoussethecat")
     runScript("../platform/firefox-tab-new-url", "https://cara.app/frimoussethecat")
 
-    # @Account2
-    runScript("../platform/firefox-cont-new-url", ARG_FFC_ID_2, "https://www.facebook.com/frimoussethecat/")
-    runScript("../platform/firefox-cont-new-url", ARG_FFC_ID_2, "https://www.youtube.com/@FrimousseTheCat")
-    runScript("../platform/firefox-cont-new-url", ARG_FFC_ID_2, "https://mastodon.social/@frimoussethecat")
-    runScript("../platform/firefox-cont-new-url", ARG_FFC_ID_2, "https://cara.app/frimoussethecat")
+    runScript("../platform/firefox-cont-new-url", CONT_INDY, "https://www.facebook.com/frimoussethecat/")
+    runScript("../platform/firefox-cont-new-url", CONT_INDY, "https://www.youtube.com/@FrimousseTheCat")
+    runScript("../platform/firefox-cont-new-url", CONT_INDY, "https://www.instagram.com/frimousse_the_cat/")
+    runScript("../platform/firefox-cont-new-url", CONT_INDY, "https://x.com/frimoussethecat/")
+    runScript("../platform/firefox-cont-new-url", CONT_INDY, "https://www.tumblr.com/frimoussethecat")
 
-    # @Account3
-    runScript("../platform/firefox-cont-new-url", ARG_FFC_ID_3, "https://www.youtube.com/@FrimousseTheCat")
-    
+    # runScript("../platform/firefox-cont-new-url", CONT_STREETBOOZE, "https://www.facebook.com/frimoussethecat/")
+    runScript("../platform/firefox-cont-new-url", CONT_STREETBOOZE, "https://www.instagram.com/frimousse_the_cat/")
+    runScript("../platform/firefox-cont-new-url", CONT_STREETBOOZE, "https://x.com/frimoussethecat/")
+    runScript("../platform/firefox-cont-new-url", CONT_STREETBOOZE, "https://www.tumblr.com/frimoussethecat")
+
     # end ***********************************************  
     runScript("../platform/windows-takescreenshot", "-success") 
 except FindFailed:
     runScript("../platform/windows-takescreenshot", "-error")
 finally:
     keyUp()
-
-# browser
-#runScript("../platform/window-close.sikuli")
