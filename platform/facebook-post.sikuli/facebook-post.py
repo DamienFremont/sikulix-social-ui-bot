6ebc4104085e4c693e4e831c7885d643226d21e7
Damien FREMONT <damien.fremont@gmail.com>
2026-09-07 10:13:26 +0200

    fix(facebook): 🐛 remove click action for deprecated next step in post submission process

--- platform/facebook-post.sikuli/facebook-post.py
+++ platform/facebook-post.sikuli/facebook-post.py
@@ -36,7 +36,6 @@ try:
     sleep(3)    
     click("facebook-post-confirm.png")
     sleep(1)
-    click("facebook-post-next2.png")
         
 finally:
     runScript("../platform/windows-takescreenshot", "-facebook")
